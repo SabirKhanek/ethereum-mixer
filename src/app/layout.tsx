4c5d1c281a1cb3b165620b3388412ab6a74832c3
Sabir Khan <sabirkhanek66@gmail.com>
2023-10-19 00:28:00 +0500

    add meta tag for google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <link rel="icon" type="image/svg+xml" href="/favicon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="google-site-verification"
+          content="ksHtd1aM4whzBNFYaw-uKSa2gZMTOZOY15LyzeR5b-s"
+        />
         <title>crypto-blender.com</title>
       </head>
       <body>
